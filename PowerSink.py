36d70cef0e6d0f0fda88d15b72b4d4a0a467473b
Planet-Moon <topwastaken@abwesend.de>
2021-06-15 00:59:58 +0200

    [refactor] remove priority attribute

--- PowerSink.py
+++ PowerSink.py
@@ -50,18 +50,9 @@ class PowerSink:
         self.name = name
         self._request_power = Power_range()
         self._allowed_power = 0
-        self._power_priority = 0
 
     def __str__(self):
         return str(self.__dict__)
-
-    @property
-    def power_priority(self):
-        return self._power_priority
-
-    @power_priority.setter
-    def power_priority(self, priority):
-        self._power_priority = priority
 
     @property
     def allowed_power(self):
